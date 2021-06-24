a79c1b4679718f3e59e3eff813da1600c52ffe21
Shreehari Jagadeesha <sjagade7@asu.edu>
2021-06-24 00:19:58 +0000

    Sync LeetCode submission - Roman to Integer (python)

--- my-folder/problems/roman_to_integer/solution.py
+++ my-folder/problems/roman_to_integer/solution.py
@@ -4,25 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-                
-        d = {'I':1,
-         'V':5,
-         'X':10,
-         'L':50,
-         'C':100,
-         'D':500,
-         'M':1000}
+        lookup={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000            
+        }   
         
-        n_prev = ans = d[s[0]]
-        for char in s[1:]:
-            n = d[char]
-            if n <= n_prev :
-                ans += n
+        n=len(s)
+        i=n-1
+        output=0
+        while i>=0:
+            if i<n-1 and lookup[s[i]]<lookup[s[i+1]]:
+                output-=lookup[s[i]]
             else:
-                ans += n - 2 * n_prev
-            n_prev = n
-        return ans
+                output+=lookup[s[i]]
+            i=i-1
         
-    
-    
-    
+        return output
+            
