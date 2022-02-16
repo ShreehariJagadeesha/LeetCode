0b38072c717a1d72078593ec2a205627cef41dd6
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-02-16 23:55:39 +0000

    Sync LeetCode submission - Container With Most Water (python)

--- my-folder/problems/container_with_most_water/solution.py
+++ my-folder/problems/container_with_most_water/solution.py
@@ -4,16 +4,6 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # temp=[]
-        # for i in range(0,len(height)):
-        #     for j in range(0,len(height)):
-        #         if i!=j:
-        #             if height[i]>height[j]:
-        #                 temp.append(height[j]*abs(i-j))
-        #             else:
-        #                 temp.append(height[i]*abs(i-j))
-        # return max(temp)
-    
         i = 0
         j = len(height) - 1
         res = 0
@@ -29,9 +19,4 @@ class Solution(object):
                 i += 1
             else:
                 j -= 1
-        return res
-                
-        
-
-            
-        
+        return res        
