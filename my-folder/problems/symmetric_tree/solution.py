21ca7a87c405860837e3af111484f7cf4a5b32a1
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-02-19 00:10:14 +0000

    Sync LeetCode submission - Symmetric Tree (python)

---
+++ my-folder/problems/symmetric_tree/solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        return self.ismirror(root.left,root.right)
+    
+    def ismirror(self,leftroot,rightroot):
+        if leftroot and rightroot:
+            return leftroot.val == rightroot.val and self.ismirror(leftroot.left,rightroot.right) and self.ismirror(leftroot.right,rightroot.left)
+        return leftroot==rightroot
