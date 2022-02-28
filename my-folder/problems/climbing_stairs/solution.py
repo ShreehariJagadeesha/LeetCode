12bfb0729981285a4cb1d2cb178f6fac98b7c93d
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-02-28 23:34:56 +0000

    Sync LeetCode submission - Climbing Stairs (python)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<4:
-            return n
-        
-        list=[0 for i in range(n+1)]
-        
-        for i in range(len(list)):
-            if(i<4):
-                list[i]=i
-            else:
-                list[i]=list[i-1]+list[i-2]
-            
-        return list[-1]
+        if n<=2: return n
+        prev1=1
+        prev2=2
+        curr=0
+        for i in range(2,n):
+            curr=prev1+prev2
+            prev1=prev2
+            prev2=curr
+        return curr
