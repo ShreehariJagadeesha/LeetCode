4f8dfdcae8befcf4e86092a156e10e6b15db3d1a
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-03-01 22:51:29 +0000

    Sync LeetCode submission - Unique Paths II (python)

--- my-folder/problems/unique_paths_ii/solution.py
+++ my-folder/problems/unique_paths_ii/solution.py
@@ -4,24 +4,23 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        m=len(obstacleGrid)
-        n=len(obstacleGrid[0])
-        
-                
-        dp=[[0 for _ in range(n)] for _ in range(m)]
-        for r in range(m):
-            if obstacleGrid[r][0]==1:break
-            dp[r][0]=1
-        
-        for c in range(n):
+        N,M=len(obstacleGrid[0]),len(obstacleGrid)
+        dp=[[0 for col in range(N)] for row in range(M)]
+        for c in range(N):
             if obstacleGrid[0][c]==1:break
-            dp[0][c]=1
+            else:
+                dp[0][c]=1
+        for r in range(M):
+            if obstacleGrid[r][0]==1:break
+            else:
+                dp[r][0]=1
                 
-        for r in range(1,m):
-            for c in range(1,n):
-                if obstacleGrid[r][c]==1:
-                    dp[r][c]=0
-                else:
-                    dp[r][c]=dp[r-1][c]+dp[r][c-1]
-        
+        for c in range(1,N):
+            for r in range(1,M):
+                if obstacleGrid[r][c]==1:continue
+                dp[r][c]=dp[r][c-1]+dp[r-1][c]
+                    
+        print dp
         return dp[-1][-1]
+            
+            
