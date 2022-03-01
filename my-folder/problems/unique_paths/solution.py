c7d1af6fba888dfa90d337bb23a0e329b993bced
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-03-01 22:29:51 +0000

    Sync LeetCode submission - Unique Paths (python)

--- my-folder/problems/unique_paths/solution.py
+++ my-folder/problems/unique_paths/solution.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if m==n==1:
-            return 1
-        dp=[[1]*m]*n
-        
-        for row in range(1,len(dp)):
-            for col in range(1,len(dp[row])):
+        dp=[[0 for col in range(m)] for row in range(n)]
+        for i in range(m):dp[0][i]=1
+        for j in range(n):dp[j][0]=1
+            
+        for row in range(1,n):
+            for col in range(1,m):
                 dp[row][col]=dp[row-1][col]+dp[row][col-1]
-        
-        return dp[-1][-1]
+                
+        return dp[n-1][m-1]
