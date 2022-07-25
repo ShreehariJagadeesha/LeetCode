3c114e5d8daaa7138b46c95a5b4f8e3cdeabf99d
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-07-25 00:55:15 +0000

    Sync LeetCode submission - Top K Frequent Elements (python)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -5,4 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        return (lambda x : [i[0] for i in x])(Counter(nums).most_common(k))
+        ans=[]
+        freq=dict()
+        
+        for n in nums:
+            if n not in freq:
+                freq[n]=1
+            else:
+                freq[n]+=1
+                
+        for key,val in freq.items():
+            if len(ans)<k:
+                heapq.heappush(ans,[val,key])
+            else:
+                heapq.heappushpop(ans,[val,key])
+                
+        return [key for value,key in ans]
