9bc4ee1cf1e4ecd704c6e448f4b0aeb692eb63fe
Shreehari Jagadeesha <sjagade7@asu.edu>
2022-07-26 23:20:37 +0000

    Sync LeetCode submission - 3Sum (python)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -4,27 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        output=[]
+        res=[]
         nums.sort()
         
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]: continue
+
+        
+        for i,a in enumerate(nums):
+            if i >0 and a==nums[i-1]:
+                continue
             l=i+1
-            r=len(nums)-1
+            r=len(nums)-1            
             
-            while(l<r):
-                if nums[i]+nums[l]+nums[r]==0:
-                    output.append([nums[i],nums[l],nums[r]])
-                    while(l<r and nums[l]==nums[l+1]):
-                        l=l+1
-                    while(l<r and nums[r]==nums[r-1]):
-                        r=r-1
+            while l<r:
+                
+                threeSum=a+nums[l]+nums[r]
+                
+                if threeSum<0:
                     l=l+1
-                    r=r-1
-                elif nums[i]+nums[l]+nums[r]>0:
+                elif threeSum>0:
                     r=r-1
                 else:
+                    res.append([a,nums[l],nums[r]])
                     l=l+1
-                    
-        
-        return output
+                    while nums[l]==nums[l-1] and l<r:
+                        l=l+1
+        return res
+                
+            
