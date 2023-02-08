ea8e85b947b4eb03b32706a44111dfdd6f881543
Shreehari Jagadeesha <sjagade7@asu.edu>
2023-02-08 00:49:29 +0000

    Sync LeetCode submission - Linked List Cycle (python3)

--- my-folder/problems/linked_list_cycle/solution.py
+++ my-folder/problems/linked_list_cycle/solution.py
@@ -1,21 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        if head==None:
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        if not head:
             return False
+        
         fast=slow=head
         while fast.next and fast.next.next:
             fast=fast.next.next
             slow=slow.next
-            if fast==slow:
+            if slow==fast:
                 return True
         return False
