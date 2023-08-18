8856db8b720de3a38b1d83535cce1ed4a31158a6
Shreehari Jagadeesha <sjagade7@asu.edu>
2023-08-18 22:31:31 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- my-folder/problems/contains_duplicate/solution.py
+++ my-folder/problems/contains_duplicate/solution.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         hashset=set()
-        
+
         for n in nums:
             if n in hashset:
                 return True
             hashset.add(n)
+        
         return False
